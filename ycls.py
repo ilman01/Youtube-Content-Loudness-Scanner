3e09a297cc3be0b832eea93b8bd3d9bd81199494
ilman01 <ilmanzidni2006@gmail.com>
2023-04-21 14:57:38 +0700

    Added Peak Loudness Reworded output

--- ycls.py
+++ ycls.py
@@ -9,21 +9,26 @@ args = parser.parse_args()
 
 inputFile = args.i
 inputLUFS = None
+inputPEAK = None
 cmdMode = None
 
 if not inputFile == None:
     inputLUFS = r128gain.get_r128_loudness([inputFile])[0]
+    inputPEAK = r128gain.scale_to_gain(r128gain.get_r128_loudness([inputFile])[1])
     cmdMode = True
 else:
     inputFile = input("Input File: ").replace('"','')
     inputLUFS = r128gain.get_r128_loudness([inputFile])[0]
+    inputPEAK = r128gain.scale_to_gain(r128gain.get_r128_loudness([inputFile])[1])
     cmdMode = False
 
-print(f"File LUFS: {inputLUFS}")
+print(f"File Loudness: {inputLUFS} LUFS")
+
+print(f"Peak Loudness: {round(inputPEAK, 1)} dBFS")
 
 ContentLoudness = round(inputLUFS + 14, 1)
 
-print(f"Youtube Content Loudness: {ContentLoudness}dB")
+print(f"Youtube Content Loudness: {ContentLoudness} dB")
 
 if cmdMode == False:
     print("Press Any Key To Exit")
